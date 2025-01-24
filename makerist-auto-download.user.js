1f6b8a3ec00be9ff8f2009f35c529175d9c86c05
apfelkuchen123 <196323756+apfelkuchen123@users.noreply.github.com>
2025-01-24 14:24:39 +0100

    Update makerist-auto-download.user.js

--- makerist-auto-download.user.js
+++ makerist-auto-download.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.makerist.de/my/meine-anleitungen*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Apfelkuchen
 // @description 24/01/2025, 12:33:12
 // @downloadUrl https://github.com/apfelkuchen123/makerist/raw/refs/heads/main/makerist-auto-download.user.js
@@ -19,21 +19,6 @@ function downloadPdfs(){
   setTimeout(nextPage, 1000);
 }
 
-function afterSeconds(waitFor, callBack){
-  let secondsCount = 15;
-
-  let dlInterval = setInterval(function(){
-    if(secondsCount>0){
-      document.getElementById('downloadPageButton').innerHTML = 'Downloading ('+secondsCount+')';
-      secondsCount -= 1;
-    }else{
-      document.getElementById('downloadPageButton').innerHTML = 'Download Page';
-      clearInterval(dlInterval);
-      downloadPdfs();
-    }
-  }, 1000)
-}
-
 function downloadAllFromPage(){
   for(let child of document.getElementsByClassName('products-wrapper')[0].children){ let url = child.lastChild.firstChild.href; let name = url.split('/')[5];
   var xhr = new XMLHttpRequest(); xhr.open('GET', 'https://www.makerist.de/pattern_pdfs/'+name+'/prepare_pdf', true); xhr.onloadend = function() { console.log(xhr.status+': '+xhr.responseText)}; xhr.send();};
@@ -41,7 +26,7 @@ function downloadAllFromPage(){
   for(let child of document.getElementsByClassName('products-wrapper')[0].children){ let url = child.lastChild.firstChild.href; let name = url.split('/')[5];
   window.open('https://www.makerist.de/pattern_zips/'+name+'/download', '_blank');};
 
-  afterSeconds(14, downloadPdfs);
+  setTimeout(downloadPdfs, 15000)
 }
 
 downloadAllFromPage();
